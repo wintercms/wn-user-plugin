7f6ac35c4ce89e478dc00da9b9e0fba2682ee4ee
Mantas Šimkūnas <mantas.simkunas@gmail.com>
2023-12-06 22:27:53 +0200

    Update partial text and queue the email

--- components/ResetPassword.php
+++ components/ResetPassword.php
@@ -89,7 +89,7 @@ class ResetPassword extends ComponentBase
             'code' => $code
         ];
 
-        Mail::send('winter.user::mail.restore', $data, function($message) use ($user) {
+        Mail::queue('winter.user::mail.restore', $data, function($message) use ($user) {
             $message->to($user->email, $user->full_name);
         });
     }
