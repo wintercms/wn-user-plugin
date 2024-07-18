8c46e099aa32f8692a648276a9ce1c76c6863ff6
Luke Towers <github@luketowers.ca>
2024-07-18 12:29:59 -0600

    Support tests in v1.1.9

--- tests/UserPluginTestCase.php
+++ tests/UserPluginTestCase.php
@@ -1,10 +1,22 @@
-<?php namespace Winter\User\Tests;
+<?php 
+
+namespace Winter\User\Tests;
 
 use App;
 use Illuminate\Foundation\AliasLoader;
 use Winter\User\Models\Settings;
 
-abstract class UserPluginTestCase extends \PluginTestCase
+if (class_exists('System\Tests\Bootstrap\PluginTestCase')) {
+    class BaseTestCase extends \System\Tests\Bootstrap\PluginTestCase
+    {
+    }
+} else {
+    class BaseTestCase extends \PluginTestCase
+    {
+    }
+}
+
+abstract class UserPluginTestCase extends BaseTestCase
 {
     /**
      * @var array   Plugins to refresh between tests.
