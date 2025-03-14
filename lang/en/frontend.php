151e533067b4db40217cbdba12213a971dcd1887
DamsFX <contact@hounddd.fr>
2025-03-14 03:31:52 +0100

    Translate frontend components

---
+++ lang/en/frontend.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    'general' => [
+        'email' => 'Email',
+        'email_placeholder' => 'Enter your email',
+        'enter_placeholder' => 'Enter your :attribute',
+        'full_name' => 'Full Name',
+        'full_name_placeholder' => 'Enter your full name',
+        'username' => 'Username',
+        'username_placeholder' => 'Enter your username',
+        'password' => 'Password',
+        'password_placeholder' => 'Choose a password',
+        'required' => 'required',
+    ],
+    'activate' => [
+        'title' => 'Your email address has not yet been verified.',
+        'infos' => 'You should verify your account otherwise it may be deleted. Please check your email to verify.',
+        'link' => 'Send the verification email again',
+    ],
+    'deactivate' => [
+        'title' => 'Deactivate your account?',
+        'button' => 'Confirm Deactivate Account',
+        'enter_password' => 'To continue, please enter your password:',
+        'infos' => 'Your account will be disabled and your details removed from the site. You can reactivate your account any time by signing back in.',
+        'link' => 'Deactivate account',
+        'mind_changed' => 'I changed my mind',
+    ],
+    'register' => [
+        'title' => 'Register',
+        'button' => 'Register',
+    ],
+    'signin' => [
+        'title' => 'Sign In',
+        'button' => 'Sign In',
+        'password_placeholder' => 'Enter your password',
+        'remember' => 'Stay logged in',
+    ],
+    'update' => [
+        'title' => 'Profile',
+        'button' => 'Save',
+        'avatar' => 'Display Picture',
+        'avatar_remove' => 'Remove current display picture',
+        'password_current' => 'Current Password',
+        'password_required' => 'To change these details, please confirm your current password.',
+        'new_password' => 'New password',
+        'new_password_confirm' => 'Confirm New Password',
+    ],
+];
---
+++ lang/fr/frontend.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    'general' => [
+        'email' => 'E-mail',
+        'email_placeholder' => 'Saisissez votre e-mail',
+        'enter_placeholder' => 'Saisissez votre :attribute',
+        'full_name' => 'Nom complet',
+        'full_name_placeholder' => 'Saisissez votre nom complet',
+        'username' => 'Pseudo',
+        'username_placeholder' => 'Entrez votre pseudo',
+        'password' => 'Mot de passe',
+        'password_placeholder' => 'Choisir un mot de passe',
+        'required' => 'requis',
+    ],
+    'activate' => [
+        'title' => 'Votre adresse e-mail n\'a pas encore été vérifiée.',
+        'infos' => 'Vous devez vérifier votre compte, faute de quoi il pourrait être supprimé. Veuillez vérifier votre courrier électronique.',
+        'link' => 'Renvoyer l\'e-mail de vérification',
+    ],
+    'deactivate' => [
+        'title' => 'Désactiver votre compte ?',
+        'button' => 'Confirmer la désactivation du compte',
+        'enter_password' => 'Pour continuer, veuillez saisir votre mot de passe actuel :',
+        'infos' => 'Votre compte sera désactivé et vos données seront supprimées du site. Vous pouvez réactiver votre compte à tout moment en vous reconnectant.',
+        'link' => 'Désactiver le compte',
+        'mind_changed' => 'J\'ai changé d\'avis',
+    ],
+    'register' => [
+        'title' => 'S\'enregistrer',
+        'button' => 'S\'enregistrer',
+    ],
+    'signin' => [
+        'title' => 'S\'authentifier',
+        'button' => 'S\'authentifier',
+        'password_placeholder' => 'Entrez votre mot de passe',
+        'remember' => 'Rester connecté',
+    ],
+    'update' => [
+        'title' => 'Profil',
+        'button' => 'Enregistrer',
+        'avatar' => 'Image de présentation',
+        'avatar_remove' => 'Supprimer l\'image de présentation actuelle',
+        'password_current' => 'Mot de passe actuel',
+        'password_required' => 'Pour modifier ces données, veuillez confirmer votre mot de passe actuel.',
+        'new_password' => 'Nouveau mot de passe',
+        'new_password_confirm' => 'Confirmer le nouveau mot de passe',
+    ],
+];
