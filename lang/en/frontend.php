b4731199a1abfaa2ac89074bd3fb409d29870694
Damien MATHIEU <contact@hounddd.fr>
2025-03-19 21:19:57 +0100

    Translate default frontend components (#56)

---
+++ lang/en/frontend.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'continue_with_password' => 'To continue, please enter your password:',
+    'password_required_update' => 'To change these details, please confirm your current password.',
+    'resend_verification_email' => 'Send the verification email again',
+    'general' => [
+        'avatar' => 'Display Picture',
+        'avatar_remove' => 'Remove current display picture',
+        'email' => 'Email',
+        'email_placeholder' => 'Enter your email',
+        'full_name' => 'Full Name',
+        'full_name_placeholder' => 'Enter your full name',
+        'login_placeholder' => 'Enter your :attribute',
+        'password' => 'Password',
+        'password_current' => 'Current Password',
+        'password_new' => 'New password',
+        'password_new_confirm' => 'Confirm New Password',
+        'password_placeholder' => 'Choose a password',
+        'profile' => 'Profile',
+        'register' => 'Register',
+        'remember' => 'Stay logged in',
+        'required' => 'required',
+        'save' => 'Save',
+        'signin' => 'Sign In',
+        'username' => 'Username',
+        'username_placeholder' => 'Enter your username',
+    ],
+    'email_not_verified' => [
+        'description' => 'You should verify your account otherwise it may be deleted. Please check your email to verify.',
+        'title' => 'Your email address has not yet been verified.',
+    ],
+    'deactivate_account' => [
+        'cancel' => 'I changed my mind',
+        'confirm' => 'Confirm Deactivate Account',
+        'description' => 'Your account will be disabled and your details removed from the site. You can reactivate your account any time by signing back in.',
+        'prompt' => 'Deactivate your account?',
+        'title' => 'Deactivate account',
+    ],
+    'signin' => [
+        'password_placeholder' => 'Enter your password',
+    ],
+];
---
+++ lang/fr/frontend.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'continue_with_password' => 'Pour continuer, veuillez saisir votre mot de passe actuel :',
+    'password_required_update' => 'Pour modifier ces données, veuillez confirmer votre mot de passe actuel.',
+    'resend_verification_email' => 'Renvoyer l\'e-mail de vérification',
+    'general' => [
+        'avatar' => 'Image de présentation',
+        'avatar_remove' => 'Supprimer l\'image de présentation actuelle',
+        'email' => 'E-mail',
+        'email_placeholder' => 'Saisissez votre e-mail',
+        'full_name' => 'Nom complet',
+        'full_name_placeholder' => 'Saisissez votre nom complet',
+        'login_placeholder' => 'Saisissez votre :attribute',
+        'password' => 'Mot de passe',
+        'password_current' => 'Mot de passe actuel',
+        'password_new' => 'Nouveau mot de passe',
+        'password_new_confirm' => 'Confirmer le nouveau mot de passe',
+        'password_placeholder' => 'Choisir un mot de passe',
+        'profile' => 'Profil',
+        'register' => 'S\'enregistrer',
+        'remember' => 'Rester connecté',
+        'required' => 'requis',
+        'save' => 'Enregistrer',
+        'signin' => 'S\'authentifier',
+        'username' => 'Pseudo',
+        'username_placeholder' => 'Saisissez votre pseudo',
+    ],
+    'email_not_verified' => [
+        'description' => 'Vous devez vérifier votre compte, faute de quoi il pourrait être supprimé. Veuillez vérifier votre courrier électronique.',
+        'title' => 'Votre adresse e-mail n\'a pas encore été vérifiée.',
+    ],
+    'deactivate_account' => [
+        'cancel' => 'J\'ai changé d\'avis',
+        'confirm' => 'Confirmer la désactivation du compte',
+        'description' => 'Votre compte sera désactivé et vos données seront supprimées du site. Vous pouvez réactiver votre compte à tout moment en vous reconnectant.',
+        'prompt' => 'Désactiver votre compte ?',
+        'title' => 'Désactiver le compte',
+    ],
+    'signin' => [
+        'password_placeholder' => 'Saisissez votre mot de passe',
+    ],
+];
